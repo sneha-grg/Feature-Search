c7ac35de37214c8b5098faddd5a5892761704eb7
Sneha Gurung <snehagurung@Snehas-Air.lan>
2024-11-13 02:31:59 -0800

    Created forward selection funciton

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
+import random
+
+def evaluation_function(feature_subset): # feature subset is a node in a graph
+    return random.uniform(0, 100)
+
+def forward_selection(features):
+    feature_set = []
+    best_overall_score = evaluation_function(feature_set)
+    hold_overall_set = []
+    print("Beginning Search.\n")
+
+    # while loop remains looping until set of features is greater than given parameter features
+    while len(feature_set) < len(features):
+        best_feature = None # will hold the best feature
+        best_feature_score = -1 # holds the lowest value so the best score can be added
+        
+        # testing each feature that has not been selected
+        for f in features:
+            if f not in feature_set:
+                new_set = feature_set + [f] # 
+                score = evaluation_function(new_set) # random score using evaluation function
+
+                print(f"\t Using feature(s) {new_set} accuracy is {score:.1f}%")
+
+                if score > best_feature_score:
+                    best_feature_score = score
+                    best_feature = f
+
+        # stop search if adding a feature doesn't improve the score
+        if best_feature is not None:
+            feature_set.append(best_feature)
+            if best_feature_score > best_overall_score:
+                best_overall_score = best_feature_score
+                hold_overall_set = feature_set.copy()
+                print(f"\nFeature set {feature_set} was best, accuracry is {best_feature_score:.1f}%")
+            elif best_feature_score < best_overall_score:
+                print("\n(Warning, Accuracy was decreased !)")
+                break
+        else:
+            break
+
+    print(f"\nFinished search!! The best feature subset is {hold_overall_set}, which has an accuracry of {best_overall_score:.1f}%")
+
+        # append for add and pop for remove
+        # feature_set.append(best_feature)
+        # print("Current set: ", feature_set)
+        # print("Best Score: ", best_feature_score)
+    
+    return feature_set
+
+
+    
 def main():
     print("Welcome to Sneha's and Anna's Feature Selection Algorithm.\n")
     num_of_features = int(input("Please enter total number of features: "))
@@ -13,8 +65,11 @@ def main():
     base_accuracy = evaluation_function([])
     print(f"\nUsing no features and 'random' evaluation, I get an accuracy of {base_accuracy:.1f} %\n")
 
+    myset = []
+    for i in range(1, num_of_features + 1):
+        myset.append(i)
+        print(i)
 
-    myset = [1, 2, 3, 4]
     if num_of_algo == '1':
         forward_selection(myset)
     else:
