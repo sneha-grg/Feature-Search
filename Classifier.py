fc8c4106389bb325482f281cfb9e79d376adcb65
sneha-grg <snehagrg14@gmail.com>
2024-12-03 21:24:26 -0800

    Finished Classifier class

--- Classifier.py
+++ Classifier.py
@@ -13,9 +13,25 @@ class Classifier:
 
     # Test method : the output is the predicted class label
     def Test(self, test_instance):
+        # we're trying to find the class label of the test instance parameter
+        # test_instance is an array of test instance features
+        # return predicted class label
 
-        # Add more code here
-        return test_instance
+        #initialize a min distance 
+        minDistance = float('inf')
+        predicted_class_label = None
+
+        for i in range(len(self.training_data)):
+            distance = 0
+            for j in range(len(test_instance)):
+                distance += (self.training_data[i][j] - test_instance[j]) ** 2
+            distance = distance ** 0.5 # this is the square root
+
+            if distance < minDistance:
+                minDistance = distance
+                predicted_class_label = self.training_labels[i]
+        
+        return predicted_class_label
 
 
 
